f067c84f2e4eec241ffe778381a1580f507962d0
ridwanalawode7 <71158802+ridwanalawode7@users.noreply.github.com>
2020-11-09 19:26:42 +0100

    Add files via upload

---
+++ custom-order.js
@@ -0,0 +1,9 @@
+$(function(){
+
+$("#drpdwnbtn").click(function(){
+$("#drpdwnbtn").toggleClass("change");
+$("#myDropdown").toggleClass("show");
+$(".dropdownMenu").toggleClass("slide");
+});
+
+});
